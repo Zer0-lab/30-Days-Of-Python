b1c4cfc3fca683b73c8ce5ba0096d481bb3c4478
Kévin <kevin.dhoust@gmail.com>
2026-02-12 13:54:01 +0100

    🧑‍💻 Day 15. Done !

---
+++ 15_Day_Python_type_errors/ex_day_15.py
@@ -0,0 +1,54 @@
+#SyntaxError
+
+print 'hello world'
+
+#NameError
+
+print(age)
+
+
+#IndexError
+
+numbers = [1, 2, 3, 4, 5]
+numbers[5]
+
+
+#ModuleNotFoundError
+
+import maths
+
+#AttributeError
+
+import maths
+
+import math
+math.PI
+math.pi
+
+
+#KeyError
+
+users = {'name':'Asab', 'age':250, 'country':'Finland'}
+users['name']
+'Asab'
+ users['county']
+
+
+#TypeError
+
+
+ 4 + '3'
+
+#ImportError
+
+from math import power
+
+
+#ValueError
+
+int('12a')
+
+
+#ZeroDivisionError
+
+ 1/0
