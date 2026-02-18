0e7825bb69a2bdf5af706dad074e26befa35af44
Kévin <kevin.dhoust@gmail.com>
2026-02-18 16:22:25 +0100

    🧑‍💻 Day 17. Done !

---
+++ 17_Day_Exception_handling/ex_day_17.py
@@ -0,0 +1,6 @@
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+
+def unpacking_five_first_countries(names):
+    return names[:5]
+
+print(unpacking_five_first_countries(names))
